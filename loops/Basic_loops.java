7f3e6153ce5b5c8d096e50fe0360024de4cd71c4
BIKRAM KUNDU <bikramkundu20122005@gmail.com>
2026-01-28 09:47:25 +0530

    Add files via upload

---
+++ loops/Basic_loops.java
@@ -0,0 +1,11 @@
+public class Basic_loops 
+{
+    public static void main(String[] args) 
+    {
+        /*i=0 => Initilization ; i<=10 ==> is condition ; i++ ==> increment */
+        for(int i = 0; i <= 10; i++)
+        {
+            System.out.println(" Good Morning everyone.");
+        }
+    }
+}
