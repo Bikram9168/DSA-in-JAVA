b87057bfd086950b8b66b5139e4f5abf67a9e812
BIKRAM KUNDU <bikramkundu20122005@gmail.com>
2026-01-30 23:40:40 +0530

    Update Composite_number.java

--- loops/Composite_number.java
+++ loops/Composite_number.java
@@ -6,19 +6,21 @@ public class Composite_number {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter the number: ");
         int num = input.nextInt();
+        boolean flag = true; //true means prime
 
         for(int i=2;i<=num-1;i++)
         {
-            if (num%i==0) 
-            {
-                System.out.println("Number is composite.");
-                break;
-            }
-            else
-            {
-                System.out.println("Prime Number.");
-                break;
-            }
+            flag = false; // false Means composite
+            break;
+        }
+        if(flag==false)
+        {
+            System.out.println("Composite Number.");
+        }
+       if(flag==true)
+        {
+            System.out.println("Prime Number.");
         }
     }
 }
+
