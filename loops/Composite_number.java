0e87250d315424f4d2affc848827cb0f217217e9
BIKRAM KUNDU <bikramkundu20122005@gmail.com>
2026-01-30 23:59:53 +0530

    Optimize composite number check using square root

--- loops/Composite_number.java
+++ loops/Composite_number.java
@@ -8,7 +8,7 @@ public class Composite_number {
         int num = input.nextInt();
         boolean flag = true; //true means prime
 
-        for(int i=2;i<=num-1;i++)
+        for(int i=2;i<=Math.sqrt(num);i++)
         {
             if (num%i==0) 
             {
@@ -30,4 +30,3 @@ public class Composite_number {
         }
     }
 }
-
