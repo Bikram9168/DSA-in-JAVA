66214c1f3dca0ef52483109e49eacf7f470ccfe2
BIKRAM KUNDU <bikramkundu20122005@gmail.com>
2026-02-01 09:19:27 +0530

    Add files via upload

---
+++ loops/While_loop_Structure.java
@@ -0,0 +1,12 @@
+public class While_loop_Structure 
+{
+    public static void main(String[] args) 
+    {
+        int i=1; // Initilization
+        while (i<=10) //Condition
+        {
+            System.out.print(i+" ");
+            i++; //Updation
+        }
+    }
+}
