143e6c7f915a0bc6a3684c9453a39ddbb284677e
BIKRAM KUNDU <bikramkundu20122005@gmail.com>
2026-02-09 13:39:37 +0530

    Uncomment Math methods in Builtin class

--- Mathods/Builtin.java
+++ Mathods/Builtin.java
@@ -1,13 +1,14 @@
 public class Builtin {
     public static void main(String[] args) {
         // Math. 
-        // System.out.println(Math.cbrt(100));
-        // System.out.println(Math.sqrt(100));
-        // System.out.println(Math.abs(-20));
-        // System.out.println(Math.ceil(8.1));
-        // System.out.println(Math.floor(-9.3));
-        // System.out.println(Math.max(2.3, 2.31));
+        System.out.println(Math.cbrt(100));
+        System.out.println(Math.sqrt(100));
+        System.out.println(Math.abs(-20));
+        System.out.println(Math.ceil(8.1));
+        System.out.println(Math.floor(-9.3));
+        System.out.println(Math.max(2.3, 2.31));
         System.out.println(Math.pow(10, 2));
         
     }
 }
+
