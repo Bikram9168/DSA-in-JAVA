b7687503356a567608e6ffc058366282000950b1
BIKRAM KUNDU <bikramkundu20122005@gmail.com>
2026-02-09 16:37:02 +0530

    Add files via upload

---
+++ Mathods/ReturnType.java
@@ -0,0 +1,14 @@
+public class ReturnType 
+{
+    public static int fun1()
+    {
+        System.out.println("Bikram");
+        System.out.println("Soham");
+        return 5;
+    }
+    public static void main(String[] args) 
+    {
+        System.out.println(4+fun1());
+        // fun1(); // Stand alone call only
+    }
+}
