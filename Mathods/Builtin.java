3d8874021f6c402696a0290ee50fa43963a49a7e
BIKRAM KUNDU <bikramkundu20122005@gmail.com>
2026-02-09 13:44:18 +0530

    Update Builtin.java

--- Mathods/Builtin.java
+++ Mathods/Builtin.java
@@ -1,14 +1,15 @@
 public class Builtin {
     public static void main(String[] args) {
         // Math. 
-        System.out.println(Math.cbrt(100));
-        System.out.println(Math.sqrt(100));
-        System.out.println(Math.abs(-20));
-        System.out.println(Math.ceil(8.1));
-        System.out.println(Math.floor(-9.3));
-        System.out.println(Math.max(2.3, 2.31));
-        System.out.println(Math.pow(10, 2));
+        System.out.println(Math.cbrt(100)); //In this function we get cube root value
+        System.out.println(Math.sqrt(100)); // In this function we gat Square root value
+        System.out.println(Math.abs(-20)); // In this function we get absolute value
+        System.out.println(Math.ceil(8.1)); //In this function we get the grater integer value
+        System.out.println(Math.floor(-9.3)); //In this function we get the small integer value
+        System.out.println(Math.max(2.3, 2.31)); // In this function we get maximum of two numbers 
+        System.out.println(Math.pow(10, 2)); // In this function first number is base and the second number is base
         
     }
 }
+
 
