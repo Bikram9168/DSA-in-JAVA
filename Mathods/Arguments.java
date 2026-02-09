c7010391d2b27cd1a62109f9270cac475df54e19
BIKRAM KUNDU <bikramkundu20122005@gmail.com>
2026-02-09 16:23:27 +0530

    Add files via upload

---
+++ Mathods/Arguments.java
@@ -0,0 +1,12 @@
+public class Arguments 
+{
+    public static void sum(int num1 , int num2)
+    /*This are the parameters*/
+    {
+        System.out.println("Sum of this two number is: "+(num1+num2));
+    }
+    public static void main(String[] args) 
+    {
+        sum(6, 8); // This are the Arguments 
+    }
+}
---
+++ Mathods/MaxNumberOfThree.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class MaxNumberOfThree {
+    public static void max(int num1,int num2, int num3)
+    {
+        if(num1>=num2 && num1>=num3) System.out.println(num1+" is the gratest.");
+        else if(num2>=num1 && num2>=num3) System.out.println(num2+" is the gratest.");
+        else System.out.println(num3+" is the gratest.");
+    }
+    public static void main(String[] args) 
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the 1st number: ");
+        int num1=input.nextInt();
+        System.out.print("Enter the 2nd number: ");
+        int num2=input.nextInt();
+        System.out.print("Enter the 3rd number: ");
+        int num3=input.nextInt();
+        max(num1, num2, num3);
+    }
+}
