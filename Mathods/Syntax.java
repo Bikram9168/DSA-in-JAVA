6dc4b2982c02c276e4621973907207d5d9c8195e
BIKRAM KUNDU <bikramkundu20122005@gmail.com>
2026-02-09 15:59:18 +0530

    Update Syntax.java

--- Mathods/Syntax.java
+++ Mathods/Syntax.java
@@ -1,10 +1,12 @@
-public class Syntax {
+public class Syntax 
+{
     public static void print()
     /*This is user defined function*/
     {
         System.out.println("Priti is a Nursing student.");
     }
     public static void main(String[] args) 
+    /*This is main function when program is run first control goes to main function */
     {
         System.out.println("Bikram is an Engineering Student");
         print(); //Here happens function call 
@@ -12,3 +14,4 @@ public class Syntax {
         
     }
 }
+
