be6a2eabda0d655cd0980d187af8065740eae9bd
BIKRAM KUNDU <bikramkundu20122005@gmail.com>
2026-02-09 16:43:31 +0530

    Update MaxNumberOfThree.java

--- Mathods/MaxNumberOfThree.java
+++ Mathods/MaxNumberOfThree.java
@@ -19,4 +19,24 @@ public class MaxNumberOfThree {
         int num3=input.nextInt();
         max(num1, num2, num3);
     }
+    // This are the two same code with differnt function
+    public static int max(int num1,int num2, int num3)
+    {
+        if(num1>=num2 && num1>=num3) return num1;
+        else if(num2>=num1 && num2>=num3) return num2;
+        else return num3;
+    }
+    public static void main(String[] args) 
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the 1st number: ");
+        int num1=input.nextInt();
+        System.out.print("Enter the 2nd number: ");
+        int num2=input.nextInt();
+        System.out.print("Enter the 3rd number: ");
+        int num3=input.nextInt();
+        System.out.println(max(num1, num2, num3));
+    }
+    
 }
+
