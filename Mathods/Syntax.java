586736c3b636a430971a8c1205acb134167e10aa
BIKRAM KUNDU <bikramkundu20122005@gmail.com>
2026-02-09 15:55:44 +0530

    Add files via upload

---
+++ Mathods/Syntax.java
@@ -0,0 +1,14 @@
+public class Syntax {
+    public static void print()
+    /*This is user defined function*/
+    {
+        System.out.println("Priti is a Nursing student.");
+    }
+    public static void main(String[] args) 
+    {
+        System.out.println("Bikram is an Engineering Student");
+        print(); //Here happens function call 
+        System.out.println("Hiya is a little girl");
+        
+    }
+}
