2d2ec85ca2c374adad1ada66e1b2869765d7b2c8
BIKRAM KUNDU <bikramkundu20122005@gmail.com>
2026-02-11 09:37:01 +0530

    Add files via upload

---
+++ ARRAY/ArrayBasics.java
@@ -0,0 +1,16 @@
+
+
+public class ArrayBasics 
+{
+    public static void main(String[] args) 
+    {
+        int[] arr = {3,4,5,6,7};
+        // Indexing
+        // Accessing the element
+        System.out.println(arr[0]); // Output => 3
+        // Updating elements
+        System.out.println(arr[2]);
+        arr[2]=8;
+        System.out.println(arr[2]);
+    }
+}
