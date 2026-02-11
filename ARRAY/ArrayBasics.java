d93861a8610c199f1c7e7a7120aed9f8d2752634
BIKRAM KUNDU <bikramkundu20122005@gmail.com>
2026-02-11 09:39:35 +0530

    Update ArrayBasics.java

--- ARRAY/ArrayBasics.java
+++ ARRAY/ArrayBasics.java
@@ -12,5 +12,12 @@ public class ArrayBasics
         System.out.println(arr[2]);
         arr[2]=8;
         System.out.println(arr[2]);
+
+        int[] arr1 = new int[4]; //Size of array is 4 | Index is 0 to 3
+        arr1[0]=10;
+        arr1[1]=20;
+        arr1[2]=30;
+        arr1[3]=40;
     }
 }
+
