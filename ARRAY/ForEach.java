38f68d223eb2094094d7ba58ccec13d009f28b11
BIKRAM KUNDU <bikramkundu20122005@gmail.com>
2026-02-12 17:21:12 +0530

    Update ForEach.java

--- ARRAY/ForEach.java
+++ ARRAY/ForEach.java
@@ -1,9 +1,13 @@
-public class ForEach {
-    public static void main(String[] args) {
+public class ForEach 
+{
+    public static void main(String[] args) 
+    {
         int[] arr={10,20,30,40,50};
+        /*In this loop we cannot change the value or update element , we can only traverse Array*/
         for(int ele : arr)
         {
             System.out.print(ele+" ");
         }
     }
 }
+
